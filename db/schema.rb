81f20ae59f94d513cba0e8dc2615cd72ef6146b9
ecmascriptguru <ecmascript.guru@gmail.com>
2017-04-24 10:25:52 -0400

    image added ot posts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424141226) do
+ActiveRecord::Schema.define(version: 20170424142331) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "caption",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "caption",            limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
 end
